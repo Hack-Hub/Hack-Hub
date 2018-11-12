d0629e43e7e1beef118c403c70201b72622d452c
David DeArmon <david.dearmon@gmail.com>
2018-11-12 09:49:23 -0600

    setup routes file

---
+++ src/Components/NewPost/NewPost.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class NewPost extends Component {
+    constructor(){
+        super()
+        this.state{
+            
+        }
+    }
+  render() {
+    return (
+      <div>
+        <h3>New Post</h3>
+        <h5>Title</h5>
+        <input />
+        <h5>URL</h5>
+        <input />
+        <button>Post</button>
+      </div>
+    );
+  }
+}
+
+export default NewPost;
