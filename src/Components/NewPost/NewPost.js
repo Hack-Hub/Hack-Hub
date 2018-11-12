861f26c6a65b0ef3f2978ede9e2d77d6e6c24331
David DeArmon <david.dearmon@gmail.com>
2018-11-12 10:41:45 -0600

    input and buttons set up

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
+import './NewPost.scss'
 
 class NewPost extends Component {
-    constructor(){
-        super()
-        this.state{
-            
-        }
+  constructor(){
+    super()
+    this.state={
+      postType:'Text',
+      title:'',
+      text:null,
+      URL:null,
+      image:null
     }
+  }
+  handleInput=(event)=>{
+    this.setState({[event.target.name]:event.target.value})
+  }
+  handleSubmit=()=>{
+
+  }
   render() {
+    const {postType} = this.state
+    //conditionally render the post type depending on the selection
+    let inputType=postType;
+    if(postType==='Text'){inputType=<div><h5>Text</h5><input name = 'text' onChange={this.handleInput}/></div>}
+    if(postType==='URL'){inputType=<div><h5>URL</h5><input name = 'URL' onChange={this.handleInput}/></div>}
+    if(postType==='Image'){inputType=<div><h5>Image</h5><input name = 'image' onChange={this.handleInput}/></div>}
+    
     return (
-      <div>
+      <div className='newPost'>
         <h3>New Post</h3>
+        <button onClick={()=>{this.setState({postType:'Text'})}}>Text</button>
+        <button onClick={()=>{this.setState({postType:'URL'})}}>URL</button>
+        <button onClick={()=>{this.setState({postType:'Image'})}}>Image</button>
         <h5>Title</h5>
-        <input />
-        <h5>URL</h5>
-        <input />
-        <button>Post</button>
+        <input name='title' onChange={this.handleInput}/>
+        {inputType}
+        <button>Submit Post</button>
       </div>
     );
   }
