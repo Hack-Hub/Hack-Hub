5eb51f43ac1cd7ab2eef41e5dff41c34e172e12d
David DeArmon <david.dearmon@gmail.com>
2018-11-14 13:41:13 -0600

    merge

--- src/Components/PostFeed/PostFeed.js
+++ src/Components/PostFeed/PostFeed.js
@@ -4,9 +4,13 @@ import './PostFeed.scss'
 class PostFeed extends Component {
   constructor() {
     super()
-
     this.state = {}
   }
+
+  componentDidMount(){
+    //to-do:axios get call
+  }
+
   render() {
     return <div className="PostFeed--container">POSTFEED</div>
   }
