40b948972ec66678b125eb3ec9cdced4272908bc
alexiastandish <alexiastandish@gmail.com>
2018-11-16 16:29:29 -0600

    merge

--- src/Components/BannerAndIconLibrary.js/IconLibrary.js
+++ src/Components/BannerAndIconLibrary.js/IconLibrary.js
@@ -4,12 +4,41 @@ import React, { Component } from 'react'
 class IconLibrary extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      selectedIcons: '',
+    }
+
+    this.setSelectedIcons = this.setSelectedIcons.bind(this)
   }
+
+  setSelectedIcons(icons) {
+    this.setState({ selectedIcons: icons })
+    this.props.setColor(this.state.selectedIcons)
+  }
+
   render() {
+    const icons = [
+      <img src="http://i67.tinypic.com/2dmiejk.jpg" alt="icon" />,
+      <img src="http://i64.tinypic.com/zl7n6a.jpg" alt="icon" />,
+      <img src="http://i67.tinypic.com/11uuohy.jpg" alt="icon" />,
+      <img src="http://i67.tinypic.com/2vmim29.jpg" alt="icon" />,
+      <img src="http://i64.tinypic.com/2q00fx5.jpg" alt="icon" />,
+      <img src="http://i65.tinypic.com/250ngk1.jpg" alt="icon" />,
+      <img src="http://i67.tinypic.com/2819e90.jpg" alt="icon" />,
+    ]
+
+    console.log('this.state', this.state)
     return (
-      <div className="">
-        <h1>IconLibrary</h1>
+      <div className="IconLibrary--container">
+        {icons.map(icon => {
+          return (
+            <div className="icons" key={icon}>
+              <button style={{ background: icon }} onClick={() => this.setSelectedIcons(icon)}>
+                {icon}
+              </button>
+            </div>
+          )
+        })}
       </div>
     )
   }
