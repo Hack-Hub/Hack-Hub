93ed06bfbf632181631a8e0081eaf75477701d29
David DeArmon <david.dearmon@gmail.com>
2018-11-26 09:38:16 -0600

    subhubs and dashboard pull up posts properly

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 import './Dashboard.scss'
+import axios from 'axios'
 import PostFeed from '../PostFeed/PostFeed'
 
 class Dashboard extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      posts:[]
+    }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios.get('/api/getPosts')
+    .then(response=>{
+      this.setState({posts:response.data})
+    })
+  }
 
   render() {
     return (
       <div className="Dashboard--container">
         <div className="Postcard--container">
-          <PostFeed />
+          <PostFeed posts={this.state.posts}/>
         </div>
       </div>
     )
